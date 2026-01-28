496def93ac36d532a37e3554f8a284c47ef76388
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-01-28 18:25:25 +0530

    Updated Qr code generation

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ import fs from "fs";
 const answer = await input({ message: "Enter the QR link text!" });
 
 let qr_svg = qr.image(answer, { type: "png" });
-qr_svg.pipe(fs.createWriteStream("qr.png"));
+qr_svg.pipe(fs.createWriteStream(`${answer}.png`));
