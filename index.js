90e7795c51da5ba80652cc61ea753ca178ba270c
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-01-31 12:53:20 +0530

    Added multiple routes

--- index.js
+++ index.js
@@ -8,5 +8,22 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Lets gooooooo!");
+  res.send(`<div>
+    <h1>Welcome to the Home Page of Our Express Server!</h1>
+    <p>This is a simple Express server running on port ${port}.</p>
+    </div>`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`<div>
+    <h1>About Us</h1>
+    <p>This server is built using Express.js to demonstrate basic routing.</p>
+    </div>`);
+});
+
+app.get("/contact", (req, res) => {
+  res.send(`<div>
+    <h1>Contact Us</h1>
+    <p>You can reach us at localhost:${port}/contact.</p>
+    </div>`);
 });
