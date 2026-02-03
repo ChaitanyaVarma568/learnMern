249f9a8b6b1de6a8e331150b1dfb0e2379d30529
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-02-03 15:36:31 +0530

    Used Morgan to log the usage

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import morgan from "morgan";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -6,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3311;
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
