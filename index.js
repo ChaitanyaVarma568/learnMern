b70a2ddc6031a04b5da6513ec281bde8ae529010
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-02-03 16:26:52 +0530

    added password check

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const _dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = 3311;
+let authenticated = false;
+
+const passwordChecker = (req, res, next) => {
+  if (req?.body?.password === "secret") authenticated = true;
+  next();
+};
 
 app.use(express.urlencoded({ extended: true }));
+app.use(passwordChecker);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
@@ -17,11 +24,8 @@ app.get("/", (req, res) => {
   res.sendFile(_dirname + "/index.html");
 });
 
-app.post("/generate", (req, res) => {
-  console.log(req.body);
-  res.send(`<section>
-      <h1>Your Generated Tag</h1>
-      <p>Name: ${req.body.name}</p>
-      <p>Tag Name: ${req.body.tag}</p>
-    </section>`);
+app.post("/check", (req, res) => {
+  if (authenticated) {
+    res.sendFile(_dirname + "/secret.html");
+  } else res.sendFile(_dirname + "/index.html");
 });
