e89adaefd745fa3c3295ead8a67a8d2279944cb8
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-02-03 16:06:31 +0530

    Sent a html bit as a response

--- index.js
+++ index.js
@@ -1,55 +1,27 @@
 import express from "express";
-import morgan from "morgan";
-import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { dirname } from "path";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
-const port = 3311;
+const PORT = 3311;
+
 app.use(express.urlencoded({ extended: true }));
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} : ${req.url} - ${new Date().toISOString()}`);
-  next();
-};
-
-app.use(logger);
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(_dirname + "/index.html");
 });
 
-app.get("/about", (req, res) => {
-  res.send(`<div>
-    <h1>About Us</h1>
-    <p>This server is built using Express.js to demonstrate basic routing.</p>
-    </div>`);
-});
-
-app.get("/contact", (req, res) => {
-  res.send(`<div>
-    <h1>Contact Us</h1>
-    <p>You can reach us at localhost:${port}/contact.</p>
-    </div>`);
-});
-
-app.post("/submit", (req, res) => {
+app.post("/generate", (req, res) => {
   console.log(req.body);
-  res.send("Form data received successfully.");
-});
-
-app.put("/update", (req, res) => {
-  res.send(200);
-});
-
-app.patch("/modify", (req, res) => {
-  res.send(200);
-});
-
-app.delete("/remove", (req, res) => {
-  res.send(200);
+  res.send(`<section>
+      <h1>Your Generated Tag</h1>
+      <p>Name: ${req.body.name}</p>
+      <p>Tag Name: ${req.body.tag}</p>
+    </section>`);
 });
