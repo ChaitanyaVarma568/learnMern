74e1cdb3a5f148151eec8e8fcda2ae7989b05ac5
Chaitanya Varma <chaitanyavarma568@gmail.com>
2026-02-04 13:16:20 +0530

    added ejs view engine

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 import express from "express";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import path, { dirname } from "path";
 
 const _dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = 3311;
-let authenticated = false;
 
-const passwordChecker = (req, res, next) => {
-  if (req?.body?.password === "secret") authenticated = true;
-  next();
-};
+app.set("view engine", "ejs");
+app.set("views", path.join(_dirname, "view"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(passwordChecker);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(_dirname + "/index.html");
+  res.sendFile(path.join(_dirname, "index.html"));
 });
 
-app.post("/check", (req, res) => {
-  if (authenticated) {
-    res.sendFile(_dirname + "/secret.html");
-  } else res.sendFile(_dirname + "/index.html");
+app.post("/greet", (req, res) => {
+  const day = (req.body.day || "").toLowerCase();
+  console.log(day);
+  res.render("greet", { day });
 });
